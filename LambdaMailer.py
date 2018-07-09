e063caf6f91a07a63ec7ed5bba65579c47d587a6
mamori017 <mamori017@outlook.jp>
2018-07-09 12:56:50 +0900

    Mod function

--- LambdaMailer.py
+++ LambdaMailer.py
@@ -3,23 +3,67 @@
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
+import urllib.parse
+import boto3
+from datetime import datetime
+import random
+import subprocess
+import json
+from collections import OrderedDict
+
+def send_mail(subject, send_from, send_to, content):
+    try:
+        sg = sendgrid.SendGridAPIClient(apikey = os.environ['SENDGRID_API_KEY'])
+        mail = Mail(Email(send_from), subject, Email(send_to), Content("text/plain", content))
+        response = sg.client.mail.send.post(request_body=mail.get())
+        
+        return response
+
+    except Exception as e:
+        print(e)
+
+def get_file(event):
+    try:
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+        file_path = '/tmp/' + '_' + datetime.now().strftime('%Y%m%d%H%M%S')
+
+        s3 = boto3.resource('s3')
+
+        bucket = s3.Bucket(bucket)
+        bucket.download_file(key, file_path)
+        
+        print('Target:' + file_path + key)
+
+        return file_path
+
+    except Exception as e:
+        print(e)
+
+def make_dict(file_path):
+    try:
+        read_file = open(file_path)
+        read_data = read_file.read()
+        read_file.close()
+        ret_dict = json.loads(read_data)
+        
+        return ret_dict
+
+    except Exception as e:
+        print(e)
 
 def lambda_handler(event, context):
- 
-    sg = sendgrid.SendGridAPIClient(apikey=os.environ['SENDGRID_API_KEY'])
+    try:
+        file_path = get_file(event)
+        s3_dict = make_dict(file_path)
+        response = send_mail(s3_dict['SUBJECT'], s3_dict['SEND_FROM'], s3_dict['SEND_TO'], s3_dict['CONTENT'])
 
-    from_email = Email(os.environ['FROM_EMAIL'])
-    to_email = Email(event['TO_EMAIL'])
-    subject = event['SUBJECT']
-    content = Content("text/plain", event['CONTENT'])
-    mail = Mail(from_email, subject, to_email, content)
-    response = sg.client.mail.send.post(request_body=mail.get())
+        if  response.status_code == 202:
+            print('E-Mail send succeeded. Status code:' + str(response.status_code))
+        else:
+            print('E-Mail send error. Status code:' + str(response.status_code))
 
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-
-    if response.status_code == 202:
-        return 'success status:' + str(response.status_code)
-    else:
-        return 'error status:' + str(response.status_code)
+        return response.status_code
+    
+    except Exception as e:
+        print(e)
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # LambdaMailer
+
+![20180705140608](https://user-images.githubusercontent.com/7507701/42428708-0c2d25dc-8370-11e8-9c8e-a7d8ac189367.png)
+
+## Overview
+
+AWS Lambda function to send emails using SendGrid.
+
+## Requirement
+
+- SendGrid API
+
+## Usage
+
+1. pip install sendgrid.
+
+1. Create new function with this code.
+
+1. Set SendGrid API key to environment valiables.
+
+1. Add S3 trigger.
+
+    |Trigger setting | |
+    |---|---|
+    |Event source bucket |Any |
+    |Event source type  |Create object or put |
+    |Prefix |Any |
+    |Suffix |Any |
+
+1. Save this function.
+
+1. Use sample file(./sample.json). Replace test file each key value.
+
+1. Upload sample file to S3 bucket.
+
+## Author
+
+[mamori017](https://github.com/mamori017)
